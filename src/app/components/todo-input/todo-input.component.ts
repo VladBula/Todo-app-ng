669db619147f21274af137b6ef3898f2d4ce0789
Vlad <vladbulynk@gmail.com>
2022-12-07 21:53:44 +0300

    todo-input

---
+++ src/app/components/todo-input/todo-input.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-input',
+  templateUrl: './todo-input.component.html',
+  styleUrls: ['./todo-input.component.scss']
+})
+export class TodoInputComponent {
+todoTitle: string = "";
+
+submitTodo(title: string){
+  this.todoTitle = title
+}
+}
